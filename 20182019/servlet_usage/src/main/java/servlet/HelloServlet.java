8e2499bf3d2a6d49d792540f2d03e677a7c0e784
gguibon <gael.guibon@gmail.com>
2018-10-09 12:51:32 +0200

    added servlet usage squelette

---
+++ 20182019/servlet_usage/src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// à remplir
+@WebServlet(name = "", urlPatterns = { "" })
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//à remplir
+	}
+
+}
---
+++ 20182019/servlet_usage/src/main/java/servlet/ServletCondition.java
@@ -0,0 +1,20 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//remplir
+@WebServlet(name = "", urlPatterns = { "" })
+public class ServletCondition extends HttpServlet {
+
+// remplir
+
+}
