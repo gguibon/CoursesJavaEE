19d4be18948503355920b8592e5ed436855a77fb
gguibon <gael.guibon@gmail.com>
2018-12-05 13:59:58 +0100

    exam clean

--- 20182019/exam/yukamu_squelette/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO food(name,brand,quality,type,favorite) values('Pulco Citron Vert','Pulco','GOOD','DRINK', false);
-INSERT INTO food(name,brand,quality,type,favorite) values('Coca-Cola Zero','The Coca-Cola Company','BAD','DRINK', false);
-INSERT INTO food(name,brand,quality,type,favorite) values('Chicken Wings','KFC','MEDIOCRE','MEAT', false);
-INSERT INTO food(name,brand,quality,type,favorite) values('Salade Iceberg','Naturalia','EXCELLENT','VEGETABLE', false);
-INSERT INTO food(name,brand,quality,type,favorite) values('Thé Vert','Lipton','EXCELLENT','DRINK', false);
